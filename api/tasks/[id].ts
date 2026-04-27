9b0bcc0ce7a40b5fd6b4f622cb6dac71d332aeab
vanessacarrion4-cpu <vanessacarrion4@gmail.com>
2026-04-28 00:16:41 +0200

    Fix PUT task-remove single()

--- api/tasks/[id].ts
+++ api/tasks/[id].ts
@@ -36,7 +36,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       .update(updates)
       .eq('id', id)
       .select()
-      .single()
+      
 
     if (error) return res.status(500).json({ error: error.message })
     return res.status(200).json({ task: data })
