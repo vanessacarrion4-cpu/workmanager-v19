9abc6acdb48e9566617493f541284bdd0a1fedcc
Vanessa Carrion <vanessacarrion4@gmail.com>
2026-05-04 23:22:59 +0200

    fix: añadir key a motion.div para AnimatePresence en subtareas

--- src/App.tsx
+++ src/App.tsx
@@ -5535,6 +5535,7 @@ function TaskCard({
         <AnimatePresence>
           {isExpanded && (
             <motion.div 
+              key={`subtasks-${task.id}`}
               initial={{ opacity: 0, height: 0 }}
               animate={{ opacity: 1, height: 'auto' }}
               exit={{ opacity: 0, height: 0 }}
